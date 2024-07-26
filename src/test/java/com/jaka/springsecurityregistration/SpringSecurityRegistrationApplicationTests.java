df175d9124cf25c2604e8da8e512c9c80b4e04ff
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-07-26 12:24:55 +0530

    Add initial project setup

---
+++ src/test/java/com/jaka/springsecurityregistration/SpringSecurityRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaka.springsecurityregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityRegistrationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
